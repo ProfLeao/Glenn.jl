33bbcdeb09290c9a8718bf9e6dba9a48e5be63e9
Reginaldo G Leao Jr <prof.reginaldo.leao@gmail.com>
2024-02-25 21:34:05 -0300

    Finalização da struct template do arquivo de dados e início do projeto da seguinte.

--- src/register.jl
+++ src/register.jl
@@ -1,4 +1,4 @@
-struct ThermoFile
+struct ThermoFileTemplate
     first_keys = [
         "SPECIE", "COMMENTS", "NT_INTERVALS", "REF_DATE_CODE", 
         "CHEM_FORM", "ST_AGREG", "MOL_W", "HEAT_FORM"
@@ -9,11 +9,23 @@ struct ThermoFile
         "CP_COEF4_",  "CP_COEF5_", "CP_COEF6_", "CP_COEF7_", 
         "B1_", "B2_"
     ]
-    first_datacols = [
-        0:16, 18:end, 
-    ]
+    first_datacols = [1:16, 17:end, 1:2, 4:9, 11:49, 51:52, 53:65, 65:end]
+    template_datacols = [
+        1:11, 12:22, 23, 24:63, 66:end, 1:16, 17:32, 33:48, 49:64,
+        65:80, 1:16, 17:32, 49:64, 65:80
+    ] 
 end
 
 struct Register
-    n_lines::Int = length(f_lines)
+    # Specie data
+    specie::String
+    data_source::String
+    num_temp_intervals::Int8
+    ref_date_code::String
+    chem_form::String
+    agreg_state::String
+    mol_weight::Float16
+    heat_form::Float16
+
+
 end
